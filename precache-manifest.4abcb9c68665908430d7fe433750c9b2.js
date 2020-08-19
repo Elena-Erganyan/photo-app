3458896758e92f1bfb3a50ce1852f3df4ea5818a
Elena-Erganyan <elena-erganyan@rambler.ru>
2020-08-19 21:43:47 +0400

    Updates

---
+++ precache-manifest.4abcb9c68665908430d7fe433750c9b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5528f461ea27f150fe55d2c9e4b81be5",
+    "url": "/photo-app/index.html"
+  },
+  {
+    "revision": "446e7e8321d64654b44b",
+    "url": "/photo-app/static/css/main.e5bf3405.chunk.css"
+  },
+  {
+    "revision": "fb231a1dd3926a5a103b",
+    "url": "/photo-app/static/js/2.b9ff086f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/photo-app/static/js/2.b9ff086f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446e7e8321d64654b44b",
+    "url": "/photo-app/static/js/main.1f636ecb.chunk.js"
+  },
+  {
+    "revision": "06f827362294a954fde1",
+    "url": "/photo-app/static/js/runtime-main.efc72e9c.js"
+  },
+  {
+    "revision": "55df549a1f27523bc1b356b0b422f18c",
+    "url": "/photo-app/static/media/preloader.55df549a.svg"
+  }
+]);
